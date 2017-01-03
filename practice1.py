669e4f8f88aa3014a7a11419d0cd133c52c95dbf
Michael <michael.bostwick@gmail.com>
2017-01-03 15:57:18 -0500

    Added string practice

--- practice1.py
+++ practice1.py
@@ -1,3 +1,37 @@
-name = raw_input('What is your name?\n')
-print name
+fruit = 'apple'
+
+def backwards(word):
+	index = 1
+	while index <= len(word):
+		letter = word[-index]
+		print letter
+		index = index + 1
 	
+# backwards(fruit)
+
+prefixes = 'JKLMNOPQ'
+suffix = 'ack'
+
+def ex2(pre,suf):
+	for letter in pre:
+		if letter in ('O', 'Q'):
+			print letter + 'u' + suf
+		else:
+			print letter + suf
+		
+# ex2(prefixes, suffix)
+
+fruit = 'banana'
+# print fruit[:]
+
+word = 'banana  '
+new_word = word.strip()
+#print new_word
+
+
+def in_both(word1, word2):
+    for letter in word1:
+        if letter in word2:
+            print letter
+            
+in_both('michael', 'sarah')
